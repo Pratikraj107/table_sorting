568ccb2aaf0ecf6d102b2c3ad9a2b5dd020b726c
Pratik Raj <praj299@gmail.com>
2023-10-02 23:20:27 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sample_app';
+  rowData: any[] =[
+    {make:'Toyota','model':'Celice','price':'45000'},
+    {make:'BMW','model':'X1','price':'55000'},
+    {make:'Audi','model':'Q3','price':'50000'},
+  ];
+
+  colDefs: ColDef[]=[
+    {field: 'make'},
+    {field: 'model'},
+    {field: 'price'}
+  ];
+
+  defaultColDef: ColDef ={
+    sortable: true, filter: true
+  }
 }
